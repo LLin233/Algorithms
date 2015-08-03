825dd95d14bb4830739958b73062acae42ff8d4f
leonard-lin <linle.job@gmail.com>
2015-08-02 22:15:51 -0700

    updates

---
+++ Valid Parentheses.java
@@ -0,0 +1,34 @@
+import java.util.*;
+/**
+*Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+*The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+*
+*/
+
+public class Solution {
+    private static final Map<Character, Character> map =  new HashMap<Character, Character>(){
+        {
+            put('(',')');
+            put('{','}');
+            put('[',']');
+        }
+    };
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        for (char c : s.toCharArray()){
+        	System.out.println(stack);
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else if (stack.isEmpty() || map.get(stack.pop()) != c) {
+                return false;
+            }
+            
+        }
+        return stack.isEmpty();
+    }
+    
+    public static void main(String[] args) {
+    	String s = "()(([]{}))";
+    	System.out.println(isValid(s));
+    }
+}
