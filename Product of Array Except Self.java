5dedf7eebcea07422469f6b464ebf99faa10707f
Leonard L <linle.job@gmail.com>
2015-08-04 00:08:36 -0700

    Update Product of Array Except Self.java

--- Product of Array Except Self.java
+++ Product of Array Except Self.java
@@ -13,29 +13,45 @@ import java.util.*;
 
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] A = new int[nums.length];
-        A[0] = nums[0];
+        // int[] A = new int[nums.length];
+        // A[0] = nums[0];
         
-        for (int i = 1; i < nums.length; i++) {
-            A[i] = nums[i] * A[i - 1]; 
-        }
+        // for (int i = 1; i < nums.length; i++) {
+        //     A[i] = nums[i] * A[i - 1]; 
+        // }
         
-        int[] B = new int[nums.length];
-        B[nums.length - 1] = nums[nums.length - 1];
+        // int[] B = new int[nums.length];
+        // B[nums.length - 1] = nums[nums.length - 1];
         
-        for (int i = nums.length - 2; i > 0; i--) {
-             B[i] = nums[i] * B[i + 1]; 
-        }
+        // for (int i = nums.length - 2; i > 0; i--) {
+        //      B[i] = nums[i] * B[i + 1]; 
+        // }
         
-        for (int i = 0; i < nums.length;i++) {
-          if (i == 0) {
-              nums[i] = B[i + 1];
-          } else if (i == nums.length - 1) {
-              nums[i] = A[i - 1];
-          } else {
-              nums[i] = A[i - 1] * B [i + 1];
-          }
-        }
-        return nums;
+        // for (int i = 0; i < nums.length;i++) {
+        //   if (i == 0) {
+        //       nums[i] = B[i + 1];
+        //   } else if (i == nums.length - 1) {
+        //       nums[i] = A[i - 1];
+        //   } else {
+        //       nums[i] = A[i - 1] * B [i + 1];
+        //   }
+        // }
+        // return nums;
+        
+        /**
+        * follow-up
+        */
+        int[] res = new int[nums.length];  
+        res[res.length-1] = 1;  
+        for(int i=nums.length-2; i>=0; i--) {  
+            res[i] = res[i+1] * nums[i+1];  
+        }  
+          
+        int left = 1;  
+        for(int i=0; i<nums.length; i++) {  
+            res[i] *= left;  
+            left *= nums[i];  
+        }  
+        return res;  
     }
 }
