a6de7600a16d58bd87984e5c31eb5d4a6b18b5fc
linle <linle.job@gmail.com>
2015-09-16 13:28:04 -0700

    update

---
+++ Word Ladder II.java
@@ -0,0 +1,99 @@
+import java.util.*;
+/**
+*   Given two words (beginWord and endWord), and a dictionary's word list, find all shortest transformation sequence(s) from beginWord to endWord, such that:
+*
+*	Only one letter can be changed at a time
+*	Each intermediate word must exist in the word list
+*	For example,
+*
+*	Given:
+*	beginWord = "hit"
+*	endWord = "cog"
+*	wordList = ["hot","dot","dog","lot","log"]
+*	Return
+*  	[
+*		["hit","hot","dot","dog","cog"],
+*    	["hit","hot","lot","log","cog"]
+*  	]
+*	Note:
+*	All words have the same length.
+*	All words contain only lowercase alphabetic characters.
+*
+*/
+
+public class Solution {
+    public List<List<String>> findLadders(String beginWord, String endWord, Set<String> wordList) {
+        List<List<String>> ladders = new ArrayList<List<String>>();
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        Map<String, Integer> distance = new HashMap<String, Integer>();
+
+        wordList.add(beginWord);
+        wordList.add(endWord);
+
+        bfs(map, distance, beginWord, endWord, wordList);
+
+        List<String> path = new ArrayList<String>();
+
+        dfs(ladders, path, endWord, beginWord, distance, map);
+
+        return ladders;
+    }
+
+    void dfs(List<List<String>> ladders, List<String> path, String crt,
+            String start, Map<String, Integer> distance,
+            Map<String, List<String>> map) {
+        path.add(crt);
+        if (crt.equals(start)) {
+            Collections.reverse(path);
+            ladders.add(new ArrayList<String>(path));
+            Collections.reverse(path);
+        } else {
+            for (String next : map.get(crt)) {
+                if (distance.containsKey(next) && distance.get(crt) == distance.get(next) + 1) { 
+                    dfs(ladders, path, next, start, distance, map);
+                }
+            }
+        }
+        path.remove(path.size() - 1);
+    }
+
+    void bfs(Map<String, List<String>> map, Map<String, Integer> distance,
+            String start, String end, Set<String> dict) {
+        Queue<String> q = new LinkedList<String>();
+        q.offer(start);
+        distance.put(start, 0);
+        for (String s : dict) {
+            map.put(s, new ArrayList<String>());
+        }
+        while (!q.isEmpty()) {
+            String crt = q.poll();
+            if (crt.equals(end)) {
+                return;
+            }
+            List<String> nextList = expand(crt, dict);
+            for (String next : nextList) {
+                map.get(next).add(crt);
+                if (!distance.containsKey(next)) {
+                    distance.put(next, distance.get(crt) + 1);
+                    q.offer(next);
+                }
+            }
+        }
+    }
+
+    List<String> expand(String crt, Set<String> dict) {
+        List<String> expansion = new ArrayList<String>();
+        for (int i = 0; i < crt.length(); i++) {
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                if (ch != crt.charAt(i)) {
+                    String expanded = crt.substring(0, i) + ch
+                            + crt.substring(i + 1);
+                    if (dict.contains(expanded)) {
+                        expansion.add(expanded);
+                    }
+                }
+            }
+        }
+        return expansion;
+    }
+}
