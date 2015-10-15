74ef417b55cd4f7dd9565b62fd1cd42c2ba0faa6
linle <linle.job@gmail.com>
2015-10-14 23:17:13 -0700

    medium

---
+++ Populating Next Right Pointers in Each Node.java
@@ -0,0 +1,31 @@
+import java.util.*;
+/**
+*   Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+*	Ifnitially, all next pointers are set to NULL.
+*	Note:
+*	You may only use constant extra space.
+*	You may assume that it is a perfect binary tree (ie, all leaves are at the same level, and every parent has two children).
+*
+*/
+
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if(root == null) {
+            return;
+        }
+
+        if(root.left != null) {
+            root.left.next = root.right;
+        }
+        if(root.right != null) {
+            root.right.next = root.next == null ? null : root.next.left;
+        }
+}
